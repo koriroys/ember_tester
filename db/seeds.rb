6c263344b2bdc36d8335a312563ec765fcd9601c
Kori Roys <kori@koriroys.com>
2013-10-30 12:00:30 -0500

    add another record to seeds file

--- db/seeds.rb
+++ db/seeds.rb
@@ -9,5 +9,6 @@
 puts "Seeding..."
 
 Post.create(title: "A Sample Post", body: "This will be a simple post record.")
+Post.create(title: "A Second Sample Post", body: "This will be our second simple post record.")
 
 puts "Complete!"
