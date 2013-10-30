409b6e8299b4e2ac970b2a8612ac2bfc19939ad5
Kori Roys <kori@koriroys.com>
2013-10-30 14:53:19 -0500

    namespace so the adapter knows what prefix to use when searching for resources

--- app/assets/javascripts/store.js
+++ app/assets/javascripts/store.js
@@ -1,5 +1,9 @@
 // http://emberjs.com/guides/models/using-the-store/
 
+DS.ActiveModelAdapter.reopen({
+  namespace: 'api/v1'
+});
+
 EmberTester.Store = DS.Store.extend({
   // Override the default adapter with the `DS.ActiveModelAdapter` which
   // is built to work nicely with the ActiveModel::Serializers gem.
