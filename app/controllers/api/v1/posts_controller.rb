2542dadfea6fa747640d194fbc3ec9e756852461
Kori Roys <kori@koriroys.com>
2013-10-30 11:54:29 -0500

    add basic functionality to posts_controller

--- app/controllers/api/v1/posts_controller.rb
+++ app/controllers/api/v1/posts_controller.rb
@@ -1,2 +1,11 @@
 class Api::V1::PostsController < ApplicationController
+  respond_to :json
+
+  def index
+    respond_with Post.all
+  end
+
+  def show
+    respond_with Post.find(params[:id])
+  end
 end
